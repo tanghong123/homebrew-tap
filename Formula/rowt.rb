4c779430095626004f9108814d8e4ae8360b7ea6
Hong Tang <hong.tang@gmail.com>
2026-07-23 23:07:02 +0800

    rowt 3.0.8 (log watchdog reload output)

--- Formula/rowt.rb
+++ Formula/rowt.rb
@@ -1,8 +1,8 @@
 class Rowt < Formula
   desc "Split traffic three ways on macOS alongside a corporate VPN"
   homepage "https://github.com/tanghong123/rowt"
-  url "https://github.com/tanghong123/rowt/archive/refs/tags/v3.0.7.tar.gz"
-  sha256 "3c6de21fbf93cd1deec8cd290bbc201213e7ce30c61031baf6af56e05fc7b6a8"
+  url "https://github.com/tanghong123/rowt/archive/refs/tags/v3.0.8.tar.gz"
+  sha256 "d489c52b9f5acae107ec435d51d374d80919033bb7aab11024ccd459dafbc088"
   license "MIT"
 
   depends_on "jq"
@@ -15,7 +15,7 @@ class Rowt < Formula
   # on Intel we still build it from source.
   on_arm do
     resource "rowt-monitor" do
-      url "https://github.com/tanghong123/rowt/releases/download/v3.0.7/rowt-monitor-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/tanghong123/rowt/releases/download/v3.0.8/rowt-monitor-aarch64-apple-darwin.tar.gz"
       sha256 "49c6e5d891e46236c790e6488a4d59f226c40f8a383279ec71e59ced56a44f22"
     end
   end
@@ -94,6 +94,6 @@ class Rowt < Formula
   end
 
   test do
-    assert_match "rowt 3.0.7", shell_output("#{bin}/rowt version")
+    assert_match "rowt 3.0.8", shell_output("#{bin}/rowt version")
   end
 end
